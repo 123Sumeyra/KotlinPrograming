07beb3e4a407e9cc7e9737ace2e0832769a82197
123Sumeyra <ozugursumeyra@gmail.com>
2022-03-19 12:54:22 +0300

    problem 2

--- KotlinProgramlama/app/src/main/java/com/sum/kotlinprogramlama/Collections.kt
+++ KotlinProgramlama/app/src/main/java/com/sum/kotlinprogramlama/Collections.kt
@@ -1,7 +1,7 @@
 package com.sum.kotlinprogramlama
 
 fun main(){
-
+/**
     //1 How many elements are printed as a result of the code below?
     val liste : ArrayList<Int> = arrayListOf(6,89,77,64,3,10)
 
@@ -13,7 +13,19 @@ fun main(){
             return@forEach
         }
     }
+**/
 
+
+    //2 What is the result?
+
+    val liste  = arrayListOf<Int>(4,2,5,2,4,12)
+    liste.sort() // [2, 2, 4, 4, 5, 12]
+    liste.removeAt(0) // [2, 4, 4, 5, 12]
+    liste.reverse()
+    println(liste) // [12, 5, 4, 4, 2]
+
+
+    //
 
 
 }
